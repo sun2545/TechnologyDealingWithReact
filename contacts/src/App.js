4893aa1c77525db1ac27f0747d38ba65c97ab1fc
sun2545 <kwonsunhye0206@gmail.com>
2018-10-12 22:53:18 -0400

    scroll with ref

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div>
-      <ScrollBox/>          
+      <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+      <button onClick={()=> this.scrollBox.scrollToBottom()}> buttom </button>          
       </div>      
     );
   }
